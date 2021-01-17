840184d404c8621119eb0f7131cfe8ac49bcd487
travis <70074725+heartaches@users.noreply.github.com>
2021-01-17 15:51:50 -0800

    Delete config.js

--- config.js
+++
@@ -1,2 +0,0 @@
-exports.token = "NzUyNjcyMDg1NzQ2Mzg0OTM2.X1bCgQ.6ZjVAkjOfph9ybfmcV3scwWP5cs"
-exports.uri = 'mongodb+srv://egg:Missiontotheloot1Uzi@cluster0.tah2q.mongodb.net/Snitch?retryWrites=true&w=majority'
